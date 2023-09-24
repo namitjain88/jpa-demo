513ec477713bb7cbe5658f71f9e503c05373ba78
Namit Jain <namit.jain88@gmail.com>
2023-09-24 15:07:32 +0530

    Used @Entity, @Table and @Id annotations to map Employee class to a database table

--- src/main/java/org/learn/JpaStarterMain.java
+++ src/main/java/org/learn/JpaStarterMain.java
@@ -3,6 +3,8 @@ package org.learn;
 public class JpaStarterMain {
 
     public static void main(String[] args) {
-        
+        Employee employee = new Employee();
+        employee.setId(1);
+        employee.setName("Foo Bar");
     }
 }
