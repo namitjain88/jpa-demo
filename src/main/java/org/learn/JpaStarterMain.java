d5f00938fdf7c9baf8b5a7033ae2bc5e42522dee
Namit Jain <namit.jain88@gmail.com>
2023-09-24 22:25:46 +0530

    Deleting a record from database using hibernate

--- src/main/java/org/learn/JpaStarterMain.java
+++ src/main/java/org/learn/JpaStarterMain.java
@@ -9,7 +9,7 @@ public class JpaStarterMain {
 
     public static void main(String[] args) {
 
-        // Start of updating existing employee object
+        // Start of deleting an existing employee object
         // 1. Create entityManagerFactory for persistenceUnitName configured in persistence.xml
         EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("persistence-test");
 
@@ -19,24 +19,20 @@ public class JpaStarterMain {
         // 3. Reading an entity data from db
         Employee employee = entityManager.find(Employee.class, 1);
 
-        // 4. Update the data using setter
-        employee.setType(EmployeeType.FULL_TIME);
-        System.out.println(employee);
-
-        // 5. Get and begin transaction
+        // 4. Get and begin transaction
         EntityTransaction transaction = entityManager.getTransaction();
         transaction.begin();
 
-        // 6. Save/persist the entity
-        entityManager.persist(employee);
+        // 5. Remove the entity
+        entityManager.remove(employee);
 
-        // 7. Commit transaction
+        // 6. Commit transaction
         transaction.commit();
 
-        // 8. Closing persistence context
+        // 7. Closing persistence context
         entityManager.close();
         entityManagerFactory.close();
-        // End of updating existing employee object
+        // End of deleting an existing employee object
 
 
         /*// Start of create and persisting employee objects
