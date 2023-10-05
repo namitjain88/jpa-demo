90bbc9bd2b842af1268e42393236f73cda3d5769
Namit Jain <namit.jain88@gmail.com>
2023-10-05 16:47:18 +0530

    Fetching custom number of columns using JPQL

--- src/main/java/org/learn/JpaJpqlDemo.java
+++ src/main/java/org/learn/JpaJpqlDemo.java
@@ -19,12 +19,25 @@ public class JpaJpqlDemo {
         );*/
 
         // Implicit JOIN due to @OneToOne: Fetch all employees with active access card
-        TypedQuery<Employee> query = entityManager.createQuery(
+        /*TypedQuery<Employee> query = entityManager.createQuery(
                 "SELECT emp FROM Employee emp WHERE emp.accessCard.isActive = true",
                 Employee.class
         );
         List<Employee> employees = query.getResultList();
-        employees.forEach(System.out::println);
+        employees.forEach(System.out::println);*/
+
+        // Fetching single field of different data type e.g. just employee name (String) or employee id (Integer)
+        TypedQuery<String> empNameQuery = entityManager.createQuery("SELECT e.name FROM Employee e", String.class);
+        TypedQuery<Integer> empIdQuery = entityManager.createQuery("SELECT e.id FROM Employee e", Integer.class);
+        List<String> empNameList = empNameQuery.getResultList();
+        List<Integer> empIdList = empIdQuery.getResultList();
+        empNameList.forEach(System.out::println);
+        empIdList.forEach(System.out::println);
+
+        // Fetching multiple fields e.g. just employee name, access card issued at
+        TypedQuery<Object[]> empCardIssuedAtQuery = entityManager.createQuery("SELECT e.name, e.accessCard.id FROM Employee e", Object[].class);
+        List<Object[]> empCardIssuedAtQueryResultList = empCardIssuedAtQuery.getResultList();
+        empCardIssuedAtQueryResultList.forEach(e -> System.out.println(e[0] + ", " + e[1]));
 
         entityManager.close();
         entityManagerFactory.close();
